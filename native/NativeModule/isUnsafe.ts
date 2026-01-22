25e979b251fbfd7904cd54fc7b7b2067e47979dc
Inrixia <Inrixia@gmail.com>
2026-01-23 06:30:31 +1300

    Add more modules to isUnsafe

--- native/NativeModule/isUnsafe.ts
+++ native/NativeModule/isUnsafe.ts
@@ -1,7 +1,7 @@
 export const DANGER_GROUPS = {
 	EXECUTION: "Execute external system commands & run background processes unrestricted with full system level access",
 	FILESYSTEM: "Full read/write/delete access to your filesystem for all files",
-	INTERNALS: "Modify the V8 engine, access internal debugging tools, or dynamically execute unverified code strings",
+	INTERNALS: "Dynamically load & execute unverified code",
 	ENVIRONMENT: "Access sensitive system info (OS users, ENV variables), control the current process, or manage application windows.",
 } as const;
 
@@ -14,6 +14,7 @@ export const isUnsafe = (moduleName: string) => {
 		case "cluster":
 		case "wasi":
 		case "WebAssembly":
+		case "repl":
 			return DANGER_GROUPS.EXECUTION;
 		case "fs":
 		case "fs/promises":
@@ -22,10 +23,18 @@ export const isUnsafe = (moduleName: string) => {
 		case "v8":
 		case "inspector":
 		case "module":
+		case "diagnostics_channel":
+		case "trace_events":
+		case "async_hooks":
+		case "domain":
+		case "ffi":
+		case "ffi-napi":
+		case "ref-napi":
 			return DANGER_GROUPS.INTERNALS;
 		case "os":
 		case "process":
 		case "electron":
+		case "perf_hooks":
 			return DANGER_GROUPS.ENVIRONMENT;
 	}
 	if (cleanName.startsWith(".") || cleanName.startsWith("file://") || PathsRegex.test(cleanName)) return DANGER_GROUPS.FILESYSTEM;
