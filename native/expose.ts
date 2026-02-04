734ef5bdeee1c1208ee33f496ce7c9517d82fdaf
Brskt <contact@brskt.be>
2026-02-04 20:45:02 +0100

    Add openExternal to lib.native
    Expose shell.openExternal for plugins to open URLs in system browser

--- native/expose.ts
+++ native/expose.ts
@@ -1,5 +1,6 @@
-import { clipboard, dialog } from "electron";
+import { clipboard, dialog, shell } from "electron";
 export const clipboardWriteText = clipboard.writeText;
+export const openExternal = shell.openExternal;
 export const showOpenDialog = dialog.showOpenDialog;
 export const showSaveDialog = dialog.showSaveDialog;
 export const showMessageBox = dialog.showMessageBox;
