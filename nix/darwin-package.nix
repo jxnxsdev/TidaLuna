3ad754103283d605c89c40ad3dca6e5edcaad79c
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-03-09 12:04:12 +0100

    test

---
+++ nix/darwin-package.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  stdenv,
+  callPackage,
+  fetchurl,
+}: let
+  injection = callPackage ./injection-darwin.nix {};
+
+  tidalDmg = fetchurl {
+    url = "https://download.tidal.com/desktop/TIDAL.arm64.dmg";
+    sha256 = "sha256-w5tQscUkhxpWOToAP4oIJJstCNFIdosebTyDI1zFIAE=";
+  };
+in
+  import ./darwin-tidal.nix {
+    prev = {inherit lib stdenv;};
+    inherit injection tidalDmg;
+  }
